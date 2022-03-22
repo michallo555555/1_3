2646671c883015703994f0f5e51a4b66bbc17e77
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-03-22 11:53:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Reszta z dzielenie przez 10!");
+    int a=15;
+    double b;
+    b=(a%10);
+    System.out.println(b);
+
+    
+  }
+}
