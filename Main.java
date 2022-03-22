8121eafd513f5c614f991eba77118d7856a6ee39
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-03-22 12:01:51 +0000

    final

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     System.out.println("Reszta z dzielenie przez 10!");
-    int a=15;
+      Scanner scan = new Scanner(System.in);
+     double a = scan.nextDouble();
+    
     double b;
     b=(a%10);
     System.out.println(b);
+  
 
     
   }
